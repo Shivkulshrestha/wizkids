46abe50b632d153aa915eadf11702ec09b4d72d6
shivkulshrestha <shiv.kulshrestha.k@gmail.com>
2020-09-01 19:43:55 +0530

    reset everything

--- wizkids/settings.py
+++ wizkids/settings.py
@@ -138,9 +138,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
