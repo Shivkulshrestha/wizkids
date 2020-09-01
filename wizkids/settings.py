6f0982dfd0093b30ae80b6ea39deeb3cb56f4dca
shivkulshrestha <shiv.kulshrestha.k@gmail.com>
2020-09-01 19:40:37 +0530

    Added static_dirs

--- wizkids/settings.py
+++ wizkids/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
