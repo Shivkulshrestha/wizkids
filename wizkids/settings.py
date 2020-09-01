8a62bece8425d84f8053b2b6ca883c398433b58f
shivkulshrestha <shiv.kulshrestha.k@gmail.com>
2020-09-02 01:31:41 +0530

    reseting

--- wizkids/settings.py
+++ wizkids/settings.py
@@ -15,7 +15,6 @@ import django_heroku
 import dj_database_url
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
