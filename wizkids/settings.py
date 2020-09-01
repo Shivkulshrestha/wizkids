59a86084e0a7a805bfa14d66b0889622ad4374a8
shivkulshrestha <shiv.kulshrestha.k@gmail.com>
2020-09-02 03:21:42 +0530

    Secret key is hidden now

--- wizkids/settings.py
+++ wizkids/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0i$8)q@y7+&=4mtdc@*(_2pz!(7+0qa02zpv=m^yp+aa@4_&p*'
+SECRET_KEY = os.environ.get('SECRET_KEY_WIZKIDS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
