cb5d8448772b76ee0def37f0be6a9e92f26f8b2c
shivkulshrestha <shiv.kulshrestha.k@gmail.com>
2020-09-09 20:51:30 +0530

    conn_max_age set to none

--- wizkids/settings.py
+++ wizkids/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=None, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
