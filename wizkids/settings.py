30e8c8430898a69a654978c6cbcfbe8b9910204e
shivkulshrestha <shiv.kulshrestha.k@gmail.com>
2020-09-19 19:57:50 +0530

    testing sendgrid

--- wizkids/settings.py
+++ wizkids/settings.py
@@ -172,10 +172,10 @@ MEDIA_URL = '/media/'
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY_WIZKIDS')
+SENDGRID_API_KEY = os.environ('SENDGRID_API_KEY_WIZKIDS')
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
